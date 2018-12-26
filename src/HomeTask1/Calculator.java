299c4893118c84d7c7f521664a65b50583f81b21
Руслан <russell.dmit@gmail.com>
2018-12-26 16:37:30 +0200

    Initial commit

---
+++ src/HomeTask1/Calculator.java
@@ -0,0 +1,51 @@
+package HomeTask1;
+
+public class Calculator {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static float add(float x, float y) {
+        return x + y;
+    }
+
+    public static double add(double x, double y) {
+        return x + y;
+    }
+
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+
+    public static float sub(float x, float y) {
+        return x - y;
+    }
+
+    public static double sub(double x, double y) {
+        return x - y;
+    }
+
+    public static int div(int x, int y) {
+        return x / y;
+    }
+
+    public static float div(float x, float y) {
+        return x / y;
+    }
+
+    public static double div(double x, double y) {
+        return x / y;
+    }
+
+    public static int mul(int x, int y) {
+        return x * y;
+    }
+
+    public static float mul(float x, float y) {
+        return x * y;
+    }
+
+    public static double mul(double x, double y) {
+        return x * y;
+    }
+}
---
+++ src/HomeTask1/Start.java
@@ -0,0 +1,17 @@
+package HomeTask1;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println(HomeTask1.Calculator.add(5, 7));
+        System.out.println(HomeTask1.Calculator.sub(100, 35));
+        System.out.println(HomeTask1.Calculator.div(200, 50));
+        System.out.println(HomeTask1.Calculator.mul(33, 3));
+        System.out.println(HomeTask1.Calculator.add(1, 3));
+        System.out.println(HomeTask1.Calculator.add(2D, 3.3));
+        System.out.println(HomeTask1.Calculator.sub(6, 2));
+        System.out.println(HomeTask1.Calculator.sub(8.4, 6.2));
+        System.out.println(HomeTask1.Calculator.div(20, 5.0));
+        System.out.println(HomeTask1.Calculator.mul(12, 4));
+        System.out.println(Calculator.mul(2.2, 11.5));
+    }
+}
